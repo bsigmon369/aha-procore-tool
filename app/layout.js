c0bf42ed5f70ce65fc3d41eb74e75ca4d898b844
bsigmon369 <bsigmon@sessasheetmetal.com>
2026-02-21 12:24:48 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,9 @@
+export const metadata = { title: "AHA Builder" };
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
