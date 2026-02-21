22ce9416c7e27497b1bc44f9fdc2ccaef248d7f3
bsigmon369 <bsigmon@sessasheetmetal.com>
2026-02-21 12:13:57 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "Arial", padding: 24 }}>
+      <h1>AHA Builder</h1>
+      <a href="/api/oauth/start">Connect Procore</a>
+    </main>
+  );
+}
