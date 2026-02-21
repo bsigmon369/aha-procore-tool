d76bd72aa9c7dd59f2e0a51d8174eb91257e1110
bsigmon369 <bsigmon@sessasheetmetal.com>
2026-02-21 13:20:22 -0500

    Create route.js

---
+++ app/api/procore/me/route.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+import { procoreFetch } from "../../../../lib/procoreAuth";
+
+export async function GET() {
+  // Procore "me" endpoint (returns the user tied to the token)
+  const resp = await procoreFetch("/rest/v1.0/me");
+  const data = await resp.json();
+  return NextResponse.json(data);
+}
